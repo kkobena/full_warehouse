22d69103054eae5bc37a236037ae7da3af857858
kkobena <badoukobena@gmail.com>
2025-05-19 11:04:44 +0200

    gestion des ticket 2 caisse avec graphics2D

--- src/main/java/com/kobe/warehouse/service/reglement/differe/service/ReglementDiffereService.java
+++ src/main/java/com/kobe/warehouse/service/reglement/differe/service/ReglementDiffereService.java
@@ -1,12 +1,18 @@
 package com.kobe.warehouse.service.reglement.differe.service;
 
 import com.kobe.warehouse.domain.enumeration.PaymentStatus;
-import com.kobe.warehouse.service.reglement.differe.dto.*;
-
+import com.kobe.warehouse.service.reglement.differe.dto.ClientDiffere;
+import com.kobe.warehouse.service.reglement.differe.dto.DiffereDTO;
+import com.kobe.warehouse.service.reglement.differe.dto.DiffereItem;
+import com.kobe.warehouse.service.reglement.differe.dto.DifferePaymentSummaryDTO;
+import com.kobe.warehouse.service.reglement.differe.dto.DiffereSummary;
+import com.kobe.warehouse.service.reglement.differe.dto.NewDifferePaymentDTO;
+import com.kobe.warehouse.service.reglement.differe.dto.ReglementDiffereReceiptDTO;
+import com.kobe.warehouse.service.reglement.differe.dto.ReglementDiffereResponse;
+import com.kobe.warehouse.service.reglement.differe.dto.ReglementDiffereWrapperDTO;
 import java.time.LocalDate;
 import java.util.Optional;
 import java.util.Set;
-
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -23,11 +29,7 @@ public interface ReglementDiffereService {
 
     Page<ClientDiffere> getClientDiffere();
 
-    Page<DiffereDTO> getDiffere(
-        Long customerId,
-        Set<PaymentStatus> paymentStatuses,
-        Pageable pageable
-    );
+    Page<DiffereDTO> getDiffere(Long customerId, Set<PaymentStatus> paymentStatuses, Pageable pageable);
 
     Optional<DiffereDTO> getOne(Long id);
 
@@ -35,30 +37,15 @@ public interface ReglementDiffereService {
 
     void printReceipt(long idReglement);
 
-    DiffereSummary getDiffereSummary(
-        Long customerId,
-        Set<PaymentStatus> paymentStatuses
-    );
+    DiffereSummary getDiffereSummary(Long customerId, Set<PaymentStatus> paymentStatuses);
 
-    DifferePaymentSummaryDTO getDifferePaymentSummary(
-        Long customerId,
-        LocalDate fromDate,
-        LocalDate toDate
-    );
+    DifferePaymentSummaryDTO getDifferePaymentSummary(Long customerId, LocalDate fromDate, LocalDate toDate);
 
-    Resource printListToPdf(Long customerId,
-                            Set<PaymentStatus> paymentStatuses);
+    Resource printListToPdf(Long customerId, Set<PaymentStatus> paymentStatuses);
 
-    Resource printReglementToPdf(Long customerId,
-                                 LocalDate fromDate,
-                                 LocalDate toDate);
+    Resource printReglementToPdf(Long customerId, LocalDate fromDate, LocalDate toDate);
 
+    Page<ReglementDiffereWrapperDTO> getReglementsDifferes(Long customerId, LocalDate fromDate, LocalDate toDate, Pageable pageable);
 
-    Page<ReglementDiffereWrapperDTO> getReglementsDifferes(
-        Long customerId,
-        LocalDate fromDate,
-        LocalDate toDate,
-        Pageable pageable
-    );
     ReglementDiffereReceiptDTO getReglementDiffereReceipt(Long id);
 }

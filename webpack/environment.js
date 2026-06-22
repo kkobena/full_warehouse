349a9e5d0247e910593410d0f42dc5eeb3120c48
kkobena <badoukobena@gmail.com>
2026-06-22 15:39:24 +0200

    build version

--- webpack/environment.js
+++ webpack/environment.js
@@ -1,5 +1,8 @@
+
+const packageVersion = require('../package.json').version;
+
 module.exports = {
   I18N_HASH: 'generated_hash',
   SERVER_API_URL: '',
-  __VERSION__: process.env.APP_VERSION || 'DEV',
+  __VERSION__: process.env.APP_VERSION || packageVersion || 'DEV',
 };

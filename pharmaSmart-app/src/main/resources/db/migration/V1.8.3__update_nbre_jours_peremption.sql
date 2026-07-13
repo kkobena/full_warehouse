e4d71d3bf25526b597ff0c6e640e22cd6ba0e6f9
kkobena <badoukobena@gmail.com>
2026-07-13 18:45:26 +0200

    refacto

--- pharmaSmart-app/src/main/resources/db/migration/V1.8.3__update_nbre_jours_peremption.sql
+++ pharmaSmart-app/src/main/resources/db/migration/V1.8.3__update_nbre_jours_peremption.sql
@@ -1,3 +1,6 @@
 
 UPDATE app_configuration set value = '90' ,value_type = 'NUMBER'   where name = 'APP_EXPIRY_ALERT_DAYS_BEFORE';
 alter table avoir_fournisseur_line   alter column prix_achat type integer using prix_achat::integer;
+
+alter table classification_criticite_log
+  alter column nouvelle_classe type varchar(10) using nouvelle_classe::varchar(10);
